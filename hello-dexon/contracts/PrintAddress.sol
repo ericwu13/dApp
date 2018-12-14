c18947e56e055f2a0c1625106b4df6a008b8ea11
eric86103 <eric86103@hotmail.com>
2018-12-14 19:44:19 +0800

    to info

--- hello-dexon/contracts/PrintAddress.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.23;
-
-contract PrintAddress {
-   event Address(address _owner);
-   function getAddress() public view {
-      emit Address(msg.sender);
-   }
-}
--- hello-dexon/migrations/2_printaddress.js
+++
@@ -1,5 +0,0 @@
-var PrintAddress= artifacts.require("./PrintAddress.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(PrintAddress);
-};
