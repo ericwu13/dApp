f6bc48195d3ca3af6679c2bf04d197f9fd090832
kevin <onedayatatime0923@gmail.com>
2018-12-15 23:06:06 +0800

    ll

--- user-dexon/contracts/Restricted.sol
+++ user-dexon/contracts/Restricted.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.25;
+import "./Ownable.sol";
 
-contract Restricted{
+contract Restricted is Ownable {
 
     event insufficientBalanceError();
     modifier onlyPositiveBalance(uint256 currentBalance, uint32 value) {
