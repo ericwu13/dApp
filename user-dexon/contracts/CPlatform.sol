3539f69c6d6a871105ca4ec957ccf155431ab38a
張問寬 <zhangwenkuan@Kuan-MacBook-Pro.local>
2018-12-16 02:23:20 +0800

    erfverf

--- user-dexon/contracts/CPlatform.sol
+++ user-dexon/contracts/CPlatform.sol
@@ -34,8 +34,8 @@ contract CPlatform is CDatabase, Restricted{
         _editName(msg.sender, name);
     }
 
-    function listProfile(address account) external view returns(string, uint256, uint32, uint32, uint32) {
-        return(_listUser(account));
+    function listProfile() external view returns(string, uint256, uint32, uint32, uint32) {
+        return(_listUser(msg.sender));
     }
 
     function post(uint32 value) external returns(uint256) {
