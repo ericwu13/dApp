386a58e227ab7a864a3a4ac7aba1468d3e8111f4
eric86103 <eric86103@hotmail.com>
2018-12-15 21:13:20 +0800

    platform

--- rx-ui/src/contract.js
+++ rx-ui/src/contract.js
@@ -12,6 +12,40 @@ class PlatformHandler {
       let platformAddress = '0x42b9991505950cc5ac38a6557d8efcdc40235de6';
       const platformContract = web3.eth.contract(platformABI).at(platformAddress);
    }
+   handleCreateUser(name) {
+      this.platformContract.methods['createUser'](name)
+      .send({from: web3.eth.accounts[0], value: value || undefined})
+      .on('confirmation', () => {})
+      .on('receipt', () => {
+         console.log(`sucessfully performed createUser`);
+      })
+      .on('error', () => console.log('unexpected error'))
+
+   }
+   handlerlistProfile() {
+      this.platformContract.methods['listProfile']()
+      .call({from: web3.eth.accounts[0]})
+      .on('receipt', () => {
+         console.log(`sucessfully performed createUser`);
+      })
+      .on('error', () => console.log('unexpected error'))
+   }
+
+   handlePost(value) {
+
+   }
+   
+   handleBuy() {
+
+   }
+
+   handleDeliever() {
+
+   }
+
+   handleDeliever() {
+
+   }
 }
 
 
