1c2e37281704f8011833a3839dad075beddeaf8b
eric86103 <eric86103@hotmail.com>
2018-12-15 22:46:17 +0800

    rxui

--- rx-ui/src/contract.js
+++ rx-ui/src/contract.js
@@ -3,49 +3,52 @@ import { PlatformABI } from './platform_abi.js'
 
 
 class PlatformHandler {
-   constructor() {
-      window.dexon.enable()
-      const dexonProvider = window.dexon
-      const web3 = new Web3(dexonProvider)
-      //web3.eth.defaultAccount = web3.eth.accounts[0]
-      let platformABI = PlatformABI;
-      let platformAddress = '0x42b9991505950cc5ac38a6557d8efcdc40235de6';
-      const platformContract = web3.eth.contract(platformABI).at(platformAddress);
-   }
-   handleCreateUser(name) {
-      this.platformContract.methods['createUser'](name)
-      .send({from: web3.eth.accounts[0], value: value || undefined})
-      .on('confirmation', () => {})
-      .on('receipt', () => {
-         console.log(`sucessfully performed createUser`);
-      })
-      .on('error', () => console.log('unexpected error'))
+    constructor() {
+        window.dexon.enable()
+        const dexonProvider = window.dexon
+        const web3 = new Web3(dexonProvider)
+        //web3.eth.defaultAccount = web3.eth.accounts[0]
+        let platformABI = PlatformABI;
+        let platformAddress = '0x42b9991505950cc5ac38a6557d8efcdc40235de6';
+        this.platformContract = web3.eth.contract(platformABI).at(platformAddress);
+    }j
+    handleCreateUser() {
+        var createUser = this.platformContract.methods['createUser']()
+        this.platformContract.methods['userProfiles'](web3.eth.accounts[0]).call().then((user) => {
+            if(user._name)
+        })
+        this.platformContract.methods['guaranteedDeposit'].call().then((value) => {
+            createUser.send({from: web3.eth.accounts[0], value: value})
+            createUser.on('receipt', function(receipt){
+            })
+        })
 
-   }
-   handlerlistProfile() {
-      this.platformContract.methods['listProfile']()
-      .call({from: web3.eth.accounts[0]})
-      .on('receipt', () => {
-         console.log(`sucessfully performed createUser`);
-      })
-      .on('error', () => console.log('unexpected error'))
-   }
+        createUser.on('error', () => console.log('unexpected error'))
+    }
+    handlerlistProfile() {
+        this.platformContract.methods['listProfile']()
+        .call({from: web3.eth.accounts[0]})
+        .on('receipt', () => {
+            console.log(`sucessfully performed createUser`);
+        })
+        .on('error', () => console.log('unexpected error'))
+    }
 
-   handlePost(value) {
+    handlePost(value) {
 
-   }
+    }
    
-   handleBuy() {
+    handleBuy() {
 
-   }
+    }
 
-   handleDeliever() {
+    handleDeliever() {
 
-   }
+    }
 
-   handleDeliever() {
+    handleDeliever() {
 
-   }
+    }
 }
 
 
